34e0e73b2afaab43a499181743e18551f035e6aa
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-01-12 18:11:14 +0100

    Automatically open a browser window

--- README.md
+++ README.md
@@ -30,8 +30,8 @@ Matchmaking system for quickly ranking huge collections of images.
 - [x] Research and pick Python web app framework
 - [x] Write initial "Hello world!"
   [Flask](https://www.palletsprojects.com/p/flask/) app
+- [x] Add script auto-reloading when source files change
 - [ ] Modify app to load filenames from sys.argv and read metadata into memory
-- [ ] Add script auto-reloading when source files change
 - [ ] Research and decide on ELO algo
 - [ ] Research and decide upon metadata format
 - [ ] Document metadata format
--- img-mm.py
+++ img-mm.py
@@ -1,4 +1,7 @@
+import os
 import sys
+import webbrowser
+import threading
 
 from pprint import pformat
 
@@ -6,9 +9,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
+APP_URL = 'http://127.0.0.1:5000/'
+
 @app.route('/')
 def hello_world():
     return pformat(sys.argv)
 
 if __name__ == '__main__':
+    # Prevent multiple browser windows being opened because of code reloading
+    # when Flask debug is active
+    if 'WERKZEUG_RUN_MAIN' not in os.environ:
+        threading.Timer(1, lambda: webbrowser.open(APP_URL)).start()
     app.run()
