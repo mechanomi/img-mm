dccc8391fda0dee7c7b4ad4967445f12821a3024
Naomi Slater <23469+mechanomi@users.noreply.github.com>
2020-01-12 17:56:27 +0100

    Print list of files passed in via sys.argv

--- img-mm.py
+++ img-mm.py
@@ -1,10 +1,14 @@
+import sys
+
+from pprint import pformat
+
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return pformat(sys.argv)
 
 if __name__ == '__main__':
     app.run()
